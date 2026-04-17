3c5cd30a9e46240cb777de899aadd5076ecfd69d
s201766550-rgb <s201766550@gmail.com>
2026-04-17 17:40:59 +0300

    Upate the trainer to remove the segmentation code

--- trainer.py
+++ trainer.py
@@ -1,77 +1,26 @@
-from ImageSegmentation_Task.kits19.kits_trainer import PfslKits
-from ImageSegmentation_Task.kits19.kits_nonkv_trainer import PfslKits as PfslKitsNonKV
-
-from ImageSegmentation_Task.IXI.ixi_trainer import IXITrainer
-from ImageSegmentation_Task.IXI.ixi_nonkv_trainer import IXITrainer as IXITrainerNonKV
-
-from ImageSegmentation_Task.ISIC2019.isic_trainer import ISICTrainer
-
 from ImageClassification_Task.ic_trainer import ICTrainer
-
-from ImageSegmentation_Task.PCam.pcam_trainer import PCamTrainer
-
-from ImageSegmentation_Task.COVID19.covid_trainer import CovidTrainer
-
-
 from utils.argparser import parse_arguments
 from pprint import pprint
 
 if __name__ == '__main__':
     args = parse_arguments()
 
+    # This prints your configuration (Split 1, CIFAR-10, etc.)
     pprint(vars(args))
 
-    if args.dataset == 'kits19':
-        if args.use_key_value_store:
-            trainer = PfslKits(args)
-        else:
-            trainer = PfslKitsNonKV(args)
-
-        trainer.fit()
-        trainer.inference()
-
-    elif args.dataset == 'IXI':
-        if args.use_key_value_store:
-            trainer = IXITrainer(args)
-        else:
-            trainer = IXITrainerNonKV(args)
-
-        trainer.fit()
-        trainer.inference()
-
-    elif args.dataset == 'ISIC2019':
-        if args.use_key_value_store:
-            trainer = ISICTrainer(args)
-        else:
-            raise NotImplementedError
-        
-        trainer.fit()
-        trainer.inference()
-
-    elif args.dataset == 'PCam':
-        if args.use_key_value_store:
-            trainer = PCamTrainer(args)
-        else:
-            raise NotImplementedError
-        
-        trainer.fit()
-
-    elif args.dataset == 'COVID19':
-        if args.use_key_value_store:
-            trainer = CovidTrainer(args)
-        else:
-            raise NotImplementedError
-        
-        trainer.fit()
-        
-    elif args.dataset == 'CIFAR10':
+    if args.dataset == 'CIFAR10':
+        # This matches your '-kv' flag
         if args.use_key_value_store:
             trainer = ICTrainer(args)
         else:
+            # Note: The paper focuses on the KV store for efficiency
             raise NotImplementedError
         
+        # This runs the Generalized and Personalized phases
         trainer.fit()
+        # This checks the final accuracy (ESL-P)
         trainer.inference()
 
     else:
-        print('invalid dataset -_-')
+        print('Kaggle Note: Only CIFAR10 is available in this fork.')
+        print('Invalid dataset -_-')
